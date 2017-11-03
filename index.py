02a8721c07946a7cedb468c18ad39e1f2e5a5963
Michael Guarascio <michael.guarascio@gmail.com>
2017-11-02 22:03:01 -0400

    batch jobs are cheaper than always on...remove worker process and do a weekly iteration of submissions and comments

--- index.py
+++ index.py
@@ -1,21 +1,44 @@
+from datetime import datetime, timedelta
 import os
 import praw
 
 def run(reddit, subreddit, substring):
     print('running...')
-    for comment in reddit.subreddit(subreddit).stream.comments():
-        print(comment.id)
-        if substring in comment.body:
-            print('_____________________________________________________________')
-            print('upvotes: ', comment.ups) # num upvotes
-            print('permalink: https://www.reddit.com', comment.permalink) # use this, but prefix with https://www.reddit.com
-            print(comment.body)
+
+    finish = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
+    start = finish - timedelta(days=7)
+    print('start: ', start)
+    print('finish: ', finish)
+ 
+    s = 0
+    for submission in reddit.subreddit(subreddit).submissions(start.timestamp(), finish.timestamp()):
+        print('submission #: ', s)
+        s = s+1
+        if substring in submission.title or substring in submission.url: 
+            print('_________________________________________________________________')
+            print(submission.title)
+            print(submission.url)
+            print('upvotes: ', submission.ups)
+            print('permalink: https://www.reddit.com', submission.permalink)
+
+        c = 0
+        submission.comments.replace_more(limit=0)
+        for comment in submission.comments.list():
+            print('comment #', c)
+            c = c+1
+            if substring in comment.body:
+                print('_____________________________________________________________')
+                print('upvotes: ', comment.ups)
+                print('permalink: https://www.reddit.com', comment.permalink)
+                print(comment.body)
 
 if __name__ == "__main__":
-    reddit = praw.Reddit(client_id=os.environ['REDDIT_CLIENT_ID'], 
-                    client_secret=os.environ['REDDIT_CLIENT_SECRET'], 
-                    user_agent='runnerreads (by /u/easy10miles)')
-    subreddit = os.environ['REDDIT_SUBREDDIT']
-    substring = os.environ['REDDIT_SEARCH_SUBSTRING']
+    print(datetime.utcnow().weekday())
+    if datetime.utcnow().weekday() == 4: # Friday only...
+        reddit = praw.Reddit(client_id=os.environ['REDDIT_CLIENT_ID'], 
+                        client_secret=os.environ['REDDIT_CLIENT_SECRET'], 
+                        user_agent='runnerreads (by /u/easy10miles)')
+        subreddit = os.environ['REDDIT_SUBREDDIT']
+        substring = os.environ['REDDIT_SEARCH_SUBSTRING']
 
-    run(reddit, subreddit, substring)
+        run(reddit, subreddit, substring)
