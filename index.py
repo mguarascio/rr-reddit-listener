19a06cb3ea75019fb091ff44b2de253caa853873
Michael Guarascio <michael.guarascio@gmail.com>
2017-11-06 21:42:49 -0500

    post to api after finding amazon product in comment or submission

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 from datetime import datetime, timedelta
 import os
 import praw
+import re
+import requests
+import sys
 
 def run(reddit, subreddit, substring):
     print('running...')
@@ -20,21 +23,50 @@ def run(reddit, subreddit, substring):
             print(submission.url)
             print('upvotes: ', submission.ups)
             print('permalink: https://www.reddit.com', submission.permalink)
+            post_comment(submission.title + ', ' + submission.url, 'https://www.reddit.com' + submission.permalink)
 
         c = 0
         submission.comments.replace_more(limit=0)
         for comment in submission.comments.list():
-            print('comment #', c)
             c = c+1
             if substring in comment.body:
                 print('_____________________________________________________________')
                 print('upvotes: ', comment.ups)
                 print('permalink: https://www.reddit.com', comment.permalink)
                 print(comment.body)
+                post_comment(comment.body, 'https://www.reddit.com' + comment.permalink)
+
+def post_comment(comment, link):
+    url = re.search("(?P<url>https://www.amazon[^\s]+[0-9a-zA-Z//])", comment).group("url")
+    print(url)
+
+    try:
+        asin = re.search("\/([A-Z0-9]{10})\/?", url).group(1)
+        print(asin)
+
+        headers = {'Authorization': 'Token ' + os.environ['RUNNERREADS_API_TOKEN']}
+
+        data = {
+            'title': 'reddit_placeholder',
+            'link': url,
+            'ASIN': asin,
+            'comments': [
+                {
+                    'text': comment,
+                    'link': link
+                }
+            ]
+        }
+        print(data)
+        response = requests.post(url=os.environ['RUNNERREADS_API'], headers=headers, json=data)
+        print(response)
+    except:
+        e = sys.exc_info()[0]
+        print('Exception posting comment: ', e)
 
 if __name__ == "__main__":
     print(datetime.utcnow().weekday())
-    if datetime.utcnow().weekday() == 4: # Friday only...
+    if datetime.utcnow().weekday() == 4: # Fridays
         reddit = praw.Reddit(client_id=os.environ['REDDIT_CLIENT_ID'], 
                         client_secret=os.environ['REDDIT_CLIENT_SECRET'], 
                         user_agent='runnerreads (by /u/easy10miles)')
