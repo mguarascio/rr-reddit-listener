bc0b5f180dc6d04c3d8898ef63c0d6d1affcc48b
Michael Guarascio <michael.guarascio@gmail.com>
2017-11-19 08:53:08 -0500

    externalize runday

--- index.py
+++ index.py
@@ -63,7 +63,7 @@ def post_comment(comment, link, score):
 
 if __name__ == "__main__":
     print(datetime.utcnow().weekday())
-    if datetime.utcnow().weekday() == 5: # Saturday
+    if datetime.utcnow().weekday() == os.environ['RUNDAY']: # 5 == Saturday
         reddit = praw.Reddit(client_id=os.environ['REDDIT_CLIENT_ID'], 
                         client_secret=os.environ['REDDIT_CLIENT_SECRET'], 
                         user_agent='runnerreads (by /u/easy10miles)')
